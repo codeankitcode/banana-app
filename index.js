94c6c02549ec5589acbeb700903f1c835b12c3dd
Ankit Kumar Roy <ankitdasout@gmail.com>
2021-12-10 10:20:44 +0530

    done

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
-let transBtn = document.getElementById("tran-btn")
-transBtn.addEventListener("click", function clickTransBtn())
+let transBtn = document.querySelector("#trans-btn")
+
+let inputText = document.querySelector("#text-input")
+
+let textOutput = document.querySelector("#text-output")
+
+let serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text
+}
+
+
+transBtn.addEventListener("click", clickTransBtn)
+
+function clickTransBtn() {
+
+    let textInput = inputText.vlaue;
+
+    fetch(getTranslationURL(textInput))
+    .then(response => response.json())
+    .then(json => {
+        let translatedText = json.contents.translated;
+        textOutput.textContent = translatedText;
+    })
+    
+}
+  
