7b1fb2572d27c237b5f625c08ded6bea6e377cfc
Ankit Kumar Roy <ankitdasout@gmail.com>
2022-12-31 14:51:09 +0530

    fixup
    javascript fixed

--- index.js
+++ index.js
@@ -1,29 +1,98 @@
-let transBtn = document.querySelector("#trans-btn")
-
-let inputText = document.querySelector("#text-input")
-
-let textOutput = document.querySelector("#text-output")
-
-let serverURL = "https://api.funtranslations.com/translate/minion.json"
+const inputEl = document.querySelector("#input-el")
+const btn = document.querySelector("#btn")
+const outputEl = document.querySelector("#output-el")
 
 
-function getTranslationURL(text){
-    return serverURL + "?" + "text=" + text
-}
+const serverUrl = "https://api.funtranslations.com/translate/minion.json"
 
 
-transBtn.addEventListener("click", clickTransBtn)
-
-function clickTransBtn() {
-
-    let textInput = inputText.vlaue;
-
-    fetch(getTranslationURL(textInput))
+function clickHandler(){
+    const inputVal = inputEl.value
+    // console.log(getTranslationURL)
+    // outputEl.textContent = inputVal
+    // console.log("my name is ankit")
+    fetch(getTranslationURL(inputVal))
     .then(response => response.json())
     .then(json => {
-        let translatedText = json.contents.translated;
-        textOutput.textContent = translatedText;
+        // let translatedText = 
+        outputEl.textContent = json.contents.translated
     })
-    
+    .catch(err => console.log("faild request", err))
 }
-  
+
+function getTranslationURL(text){
+    return serverUrl + "?text=" + text
+}
+
+btn.addEventListener("click", clickHandler)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let transBtn = document.querySelector("#trans-btn")
+
+
+// let inputText = document.querySelector("#text-input")
+
+// let textOutput = document.querySelector("#text-output")
+
+// let serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+// function getTranslationURL(text){
+//     return serverURL + "?" + "text=" + text
+// }
+
+
+// transBtn.addEventListener("click", clickTransBtn)
+
+// function clickTransBtn() {
+
+//     let textInput = inputText.vlaue;
+
+//     fetch(getTranslationURL(textInput))
+//     .then(response => response.json())
+//     .then(json => {
+//         let translatedText = json.contents.translated;
+//         textOutput.textContent = translatedText;
+//     })
+//     .catch(err => console.log("failed", err))
+    
+// }
